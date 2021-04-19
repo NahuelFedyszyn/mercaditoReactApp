b380dda5a238cabce672640018b9666845550f01
Nahuel Fedyszyn <32500578+NahuelFedyszyn@users.noreply.github.com>
2021-04-19 10:40:56 -0300

    19/04 Version inicial terminada

--- src/App.js
+++ src/App.js
@@ -32,39 +32,16 @@ const ABC = [
 ]
 
 
-/*
-------------------------------------------
-Esta funcion se usa para mover el indice del array ABC
-Necesita ser llamada de forma recurrente (usando setInterval),
-pero solo si isRunning es igual a true
-
-
-function move(){
-  if (stateIndex<ABC.length){
-    setStateIndex(stateIndex + 1);
-  }
-  else{
-    setStateIndex(0)
-  }
-}
--------------------------------------------
-
-
-
- */
-
-
-//Componente ----------------------
 function App() {
   
   const [stateIndex, setStateIndex] = useState(0);
   const [selected, setSelected] = useState(ABC[stateIndex]);
   const [isRunning, setIsRunning] = useState(false);
-  const [timerWidth, setTimerWidth] = useState(0.1);
+  
   
   useEffect(() => {
     if(isRunning){
-      setTimerWidth(0.00)
+      
       const interval = setInterval(() => {
         if(stateIndex<ABC.length-1){
           setStateIndex(stateIndex + 1);
@@ -73,63 +50,22 @@ function App() {
           setStateIndex(0);
         }
         setSelected(ABC[stateIndex])
-        //console.log(`${stateIndex}  --  ${ABC[stateIndex]}`)
+        
       }, 10);
       
       return () => clearInterval(interval);
     }
   });
-
-  //Este useEffect se usaba para un timer, despues me acorde que el mercadito no se juega con un timer...
-  /*useEffect(() => {
-    if(!isRunning && timerWidth<99.9){
-      const interval = setInterval(() => {
-        setTimerWidth(timerWidth + 0.10)
-      }, 10);
-      
-      return () => clearInterval(interval);
-    }
-  });
-*/
-
-  function handleClick(){
-    if(!isRunning){
-      //alert("Running");
-      setIsRunning(true);
-      
-    }
-    else{
-      //alert("Stopped");
-      setIsRunning(false);
-      
-    }
-    
-    
-
-  }
-
-  let timerStyle = {
-    width:`${timerWidth}%`
-  }
-
 
   return (
-    <>
+    
       <div>
         <div className="MainContainer">
-          <button onClick={handleClick} className="LetterContainer">
+          <button onClick={()=>{setIsRunning(!isRunning)}} className="LetterContainer">
             <span>{selected}</span>
           </button>
         </div>
       </div>
-
-
-      {/* El div para el timer que al final no se uso */}
-      {/*<div className="timerBox">
-        <div className="timer" style={timerStyle}></div>
-  </div>*/}
-      
-    </>
   );
 }
 
